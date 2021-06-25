630eec4d8b06f9e4dfb2639b833eda18411d5b4a
Cyril MORISSE <cmorisse@boxes3.net>
2021-06-25 12:38:33 +0200

    FIX: message_processing index

--- models/message_processing.py
+++ models/message_processing.py
@@ -85,7 +85,7 @@ class IMQMessageProcessingLog(models.Model):
 
     processing_id = fields.Many2one('imq.message_processing', _("Processing"), ondelete='cascade', index=True)
     message_id = fields.Many2one('imq.message', _("Message"), ondelete='cascade', index=True)
-    active_message_id = fields.Many2one('imq.message', _("Active Message"), ondelete='cascade', inndex=True)
+    active_message_id = fields.Many2one('imq.message', _("Active Message"), ondelete='cascade', index=True)
 
     logger_name = fields.Char()
     log_level = fields.Selection(IMQ_MESSAGE_PROCESSOR_LOG_LEVEL)

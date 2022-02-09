dfd82245485b557b3efac5b925fb683b36c499b1
Cyril MORISSE <cmorisse@boxes3.net>
2022-02-09 10:32:17 +0100

    IMP: ir_cron_patcher - log level is now warning

--- ir_cron_patcher.py
+++ ir_cron_patcher.py
@@ -15,7 +15,7 @@ _logger = logging.getLogger('ir_cron_patcher')
 
 def patch_ir_cron():
     ir_cron._intervalTypes['seconds'] = lambda interval: relativedelta(seconds=interval)
-    _logger.warning("Added 'seconds' to ir_cron.py::_intervalTypes")
+    _logger.info("Added 'seconds' to ir_cron.py::_intervalTypes")
 
     server.SLEEP_INTERVAL = 10
-    _logger.warning("server.py::SLEEP_INTERVAL=%s", server.SLEEP_INTERVAL)
+    _logger.info("server.py::SLEEP_INTERVAL=%s", server.SLEEP_INTERVAL)

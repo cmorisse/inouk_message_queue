2f8ea64b1dad56e59b0297c505aa89f5971fa630
Cyril MORISSE <cmorisse@boxes3.net>
2025-01-15 19:22:39 +0100

    IMP: Add timestamp on Odoo notifications

--- models/queue__odoo.py
+++ models/queue__odoo.py
@@ -13,6 +13,7 @@ import slackdown
 import odoo
 from odoo import models, fields, api
 from odoo.exceptions import except_orm
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.translate import _
 from odoo.tools.safe_eval import safe_eval
 
@@ -48,7 +49,8 @@ class IMQOdooQueue(models.Model):
             if record.use_odoo_notifications:
                 body_html = slackdown.render(message or "")
                 if message_obj:
-                    obj_url = f'Message: <a href="{message_obj.get_form_url()}">{message_obj.name}</a>'
+                    _now = datetime.datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+                    obj_url = f'Message: <a href="{message_obj.get_form_url()}">{message_obj.name}</a> <br> At: {_now}'
                     body_html += obj_url
                 if user_obj is None:
                     user_obj = self.env.user
